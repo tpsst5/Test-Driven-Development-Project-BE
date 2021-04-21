c809afcd9bac848fc1e7fd87a90bc5e08f3f51a2
tpsst5 <tshea31@gmail.com>
2021-04-21 15:20:20 -0600

    Initial commit.

--- src/main/java/com/TDDTutorial/model/Book.java
+++ src/main/java/com/TDDTutorial/model/Book.java
@@ -7,4 +7,15 @@ import javax.persistence.Entity;
 @Data
 @Entity
 public class Book {
+    // Instance variables
+    private int id;
+    private String title, author, genre, publisher;
+
+    public Book() {
+        id = 0;
+        title = "";
+        author = "";
+        genre = "";
+        publisher = "";
+    }
 }
